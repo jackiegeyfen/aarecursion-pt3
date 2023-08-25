2c85b1d09b5cc185cf3ab1fd2165a0c1a2657297
Jacqueline <jgeyfen@gmail.com>
2023-08-24 20:08:39 -0700

    problem 11

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -1,21 +1,33 @@
-/***********************************************************************
-Write a recursive function called `flatten` that takes a single array with
-any number of nested arrays and returns and array with all the nested
-contents on one level.
 
-Do not use the built in Array.flat.
+// Write a recursive function called `flatten` that takes a single array with
+// any number of nested arrays and returns and array with all the nested
+// contents on one level.
 
-Examples:
+// Do not use the built in Array.flat.
 
-flatten([]); // []
-flatten([1, 2]); // [1, 2]
-flatten([1, [2, [3]]]); // [1, 2, 3]
-***********************************************************************/
+// Examples:
+
+// base case - when you have an empty array
+
+//recursive case - if the array contains arrays
+
+//recursive step- call function within array argument --> return flatten arr.push -->
 
 function flatten(arr) {
-  // Your code here 
+  let returnArr=[];
+  for (let el of arr){
+    if (Array.isArray(el)){
+      // let currArr= flatten(el);
+      returnArr=returnArr.concat(flatten(el));
+    }
+    else returnArr.push(el);
+  }
+  return returnArr;
 }
-  
+
+// flatten([]); // []
+console.log(flatten([1, 2])); // [1, 2]
+// flatten([1, [2, [3]]]); // [1, 2, 3]
+// ***********************************************************************/
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = flatten;
-  
